f8c8498dc5bf30b0ef2e2d4b71392918853753bb
trmylinh <linhninh2@gmail.com>
2024-02-29 22:01:49 +0700

    ui base voucher tab

---
+++ app/src/main/java/com/example/thecoffee/data/models/Bean.kt
@@ -0,0 +1,11 @@
+package com.example.thecoffee.data.models
+
+data class Bean (
+    val id: String? = null,
+    val title: String? = null,
+    val desc: String? = null,
+    val amountBean: Number? = null,
+    val startDate: String? = null,
+    val endDate: String? = null,
+    val hsd: Number? = null
+)
